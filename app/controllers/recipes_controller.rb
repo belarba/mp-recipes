b2eff2fbda6337f5b787ed1864393d181c83a667
Daniel Belarmino <belarba@gmail.com>
2021-11-01 17:48:53 +0000

    only fetch if needs

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,12 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = helpers.contentful.entries(content_type:'recipe', include: 2)
+    @@recipes ||= helpers.contentful.entries(content_type:'recipe', include: 2)
   end
 
   def show
-    @recipes = helpers.contentful.entries(content_type:'recipe', include: 2)
-    @recipe = @recipes.find { |r|  r.id == params[:id] }
+    @@recipes ||= helpers.contentful.entries(content_type:'recipe', include: 2)
+    @recipe = @@recipes.find { |r|  r.id == params[:id] }
   end
 
 end
